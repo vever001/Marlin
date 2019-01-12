332bc20f34fb6165e7b5797a759a5dec89c0096c
Herve Donner <hervedonner@gmail.com>
2019-01-12 01:20:10 +0100

    Hotend PID tuning and Bed thermal protection tuning

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -379,10 +379,15 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
+  // Herve's hotend
+  #define DEFAULT_Kp 52.61
+  #define DEFAULT_Ki 7.06
+  #define DEFAULT_Kd 98.04
+
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  //#define DEFAULT_Kp 22.2
+  //#define DEFAULT_Ki 1.08
+  //#define DEFAULT_Kd 114
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
